65504eb563a14c9079c37dd065bfe1bc3b45ead0
vagrant <vagrant@kellogg-dev-box>
2014-05-15 03:23:28 +0000

    upload model and first DB run

---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140515025835) do
+
+  create_table "contacts", force: true do |t|
+    t.string  "Type"
+    t.string  "Location"
+    t.string  "Company_name"
+    t.string  "Contact"
+    t.string  "Address"
+    t.integer "Phone_number"
+    t.string  "Website"
+    t.integer "Trip_id"
+  end
+
+  create_table "countries", force: true do |t|
+    t.string  "Name"
+    t.string  "Picture_url"
+    t.integer "Continent_id"
+  end
+
+  create_table "itineraries", force: true do |t|
+    t.integer "Day"
+    t.string  "City"
+    t.text    "Activity"
+    t.string  "Hotel"
+    t.string  "Transportation"
+    t.string  "Itinerary_url"
+    t.integer "Trip_id"
+  end
+
+  create_table "regions", force: true do |t|
+    t.string "Name"
+    t.string "Picture_url"
+  end
+
+  create_table "trips", force: true do |t|
+    t.string  "Name"
+    t.string  "Picture_url"
+    t.string  "Description"
+    t.string  "Itinerary_url"
+    t.string  "Budget_url"
+    t.string  "Priority_url"
+    t.integer "Country_id"
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+Region.delete_all
+Region.create("Name" => "North America", "Picture_url" => "http://th07.deviantart.net/fs71/PRE/f/2012/198/b/a/the_united_states_of_america_by_wire4k-d57jle2.png")
+Region.create("Name" => "Europe", "Picture_url" => "http://www.mappi.net/img/carte_europe/carte-europe-ville.jpg")
+Region.create("Name" => "Carribbean", "Picture_url" => "http://www.worldatlas.com/webimage/countrys/caribnew.gif")
+Region.create("Name" => "Central and South America", "Picture_url" => "http://www.gilgordon.com/images/pics/map_samerica.gif")
+Region.create("Name" => "Asia", "Picture_url" => "http://www.asap-asia.org/images/asia-map-dr-shilpa.jpg")
+Region.create("Name" => "Africa", "Picture_url" => "http://www.worldatlas.com/webimage/countrys/africapolitmap.jpg")
+Region.create("Name" => "Australia", "Picture_url" => "http://www.worldatlas.com/webimage/countrys/oceania/lgcolor/aunewcolor.gif")
+Region.create("Name" => "Antartica", "Picture_url" => "http://www.mapsofworld.com/antarctica/maps/antarctica-map.gif")
+
+
+
+
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
