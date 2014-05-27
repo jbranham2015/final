53873729da8a0bf0450757d771bb695b2d8527cf
vagrant <vagrant@kellogg-dev-box>
2014-05-27 18:57:04 +0000

    updated some formatting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527161711) do
+ActiveRecord::Schema.define(version: 20140527175339) do
 
   create_table "contacts", force: true do |t|
     t.string  "Type"
